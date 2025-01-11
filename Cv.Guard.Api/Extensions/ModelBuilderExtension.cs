02f87da860615c5486edf9a9d40857b7b866338d
Moshele Seoka <seokamk@yahoo.com>
2025-01-11 22:19:14 +0200

    Refactor ConfigureRelations method for improved readability and formatting

--- Cv.Guard.Api/Extensions/ModelBuilderExtension.cs
+++ Cv.Guard.Api/Extensions/ModelBuilderExtension.cs
@@ -5,7 +5,8 @@ namespace Cv.Guard.Api.Extensions
 {
 	public static class ModelBuilderExtension
 	{
-		public static ModelBuilder ConfigureRelations(this ModelBuilder builder){
+		public static ModelBuilder ConfigureRelations(this ModelBuilder builder)
+		{
 			builder.ApplyConfiguration(new EmailConfiguration());
 			builder.ApplyConfiguration(new LocationConfiguration());
 			builder.ApplyConfiguration(new UploadConfiguration());
