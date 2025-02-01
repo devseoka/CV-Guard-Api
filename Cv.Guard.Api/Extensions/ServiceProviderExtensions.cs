f0a6fa13caa63c9765fea773f5d1d90525ed76eb
Moshele Seoka <seokamk@yahoo.com>
2025-02-01 22:55:01 +0200

    Rename method to EnsureDatabaseCreated and invoke it in Program.cs for database initialization

--- Cv.Guard.Api/Extensions/ServiceProviderExtensions.cs
+++ Cv.Guard.Api/Extensions/ServiceProviderExtensions.cs
@@ -4,7 +4,7 @@ namespace Cv.Guard.Api.Extensions
 {
 	public static class ServiceProviderExtensions
 	{
-        public static IServiceProvider EnsureDataCreated(this IServiceProvider services){
+        public static IServiceProvider EnsureDatabaseCreated(this IServiceProvider services){
             
             using var scope = services.CreateScope();
             var context = scope.ServiceProvider.GetRequiredService<CvgaContext>();
