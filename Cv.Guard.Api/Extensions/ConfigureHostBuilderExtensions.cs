0f31088fc856350e399f7a4d4d3d9c48b5da07ba
Moshele Seoka <seokamk@yahoo.com>
2025-02-02 13:09:33 +0200

    Refactor ConfigureSerilog method for improved readability and maintainability

--- Cv.Guard.Api/Extensions/ConfigureHostBuilderExtensions.cs
+++ Cv.Guard.Api/Extensions/ConfigureHostBuilderExtensions.cs
@@ -3,47 +3,47 @@ using Serilog.Events;
 using Serilog.Sinks.MSSqlServer;
 using System.Data;
 
-public static class ConfigureHostBuilderExtensions
+namespace Cv.Guard.Api.Extensions
 {
-	/// <summary>
-	/// Configures Serilog for the WebApplicationBuilder with specified connection string.
-	/// </summary>
-	/// <param name="builder">The WebApplicationBuilder to configure.</param>
-	/// <param name="connection">The connection string for the SQL Server database.</param>
-	/// <returns>The configured WebApplicationBuilder.</returns>
-	public static WebApplicationBuilder ConfigureSerilog(this WebApplicationBuilder builder, string connection)
+	public static class ConfigureHostBuilderExtensions
 	{
-		var columnOptions = new ColumnOptions
+		public static WebApplicationBuilder ConfigureSerilog(this WebApplicationBuilder builder, string connection)
 		{
-			Exception = { ColumnName = "ExceptionDetails" },
-			Level = { ColumnName = "LogLevel", DataType = SqlDbType.NVarChar, DataLength = 50 },
-			PrimaryKey = { ColumnName = "Id", DataType = SqlDbType.UniqueIdentifier },
-			MessageTemplate = { ColumnName = "MessageTemplate", DataType = SqlDbType.NVarChar, DataLength = -1 },
-			Message = { ColumnName = "Message", DataType = SqlDbType.NVarChar, DataLength = -1 }
-		};
+			var columnOptions = new ColumnOptions
+			{
+				Exception = { ColumnName = "ExceptionDetails" },
+				Level = { ColumnName = "LogLevel", DataType = SqlDbType.NVarChar, DataLength = 50 },
+				PrimaryKey = { ColumnName = "Id", DataType = SqlDbType.UniqueIdentifier },
+				MessageTemplate = { ColumnName = "MessageTemplate", DataType = SqlDbType.NVarChar, DataLength = -1 },
+				Message = { ColumnName = "Message", DataType = SqlDbType.NVarChar, DataLength = -1 }
+			};
 
-		var sinkOptions = new MSSqlServerSinkOptions
-		{
-			AutoCreateSqlTable = true,
-			TableName = "Logs",
-			SchemaName = "audit",
-		};
+			var sinkOptions = new MSSqlServerSinkOptions
+			{
+				AutoCreateSqlTable = true,
+				TableName = "Logs",
+				SchemaName = "audit",
+			};
 
-		Serilog.Debugging.SelfLog.Enable(Console.Error);
+			// Enable Serilog self-logging (useful for debugging)
+			Serilog.Debugging.SelfLog.Enable(Console.Error);
 
-		builder.Host.UseSerilog((context, services, loggerConfiguration) =>
-			loggerConfiguration
-				.Enrich.FromLogContext()
-				.Enrich.WithProperty("MachineId", Environment.MachineName)
-				.WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Debug)
-				.WriteTo.MSSqlServer(
-					connectionString: connection,
-					sinkOptions: sinkOptions,
-					columnOptions: columnOptions,
-					restrictedToMinimumLevel: LogEventLevel.Error
-				)
-		);
+			// Configure Serilog
+			builder.Host.UseSerilog((context, services, loggerConfiguration) =>
+				loggerConfiguration
+					.Enrich.FromLogContext()
+					.Enrich.WithProperty("MachineId", Environment.MachineName)
+					.WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Debug)
+					.WriteTo.MSSqlServer(
+						connectionString: connection,
+						sinkOptions: sinkOptions,
+						columnOptions: columnOptions,
+						restrictedToMinimumLevel: LogEventLevel.Error
+					)
+			);
 
-		return builder;
+			return builder;
+		}
+
 	}
 }
