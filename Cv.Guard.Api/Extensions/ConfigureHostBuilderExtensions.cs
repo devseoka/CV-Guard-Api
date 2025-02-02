c62018b8f25c43f7a26a8028c3c6adbe0a10d3d6
Moshele Seoka <seokamk@yahoo.com>
2025-02-02 13:00:13 +0200

    Refactor ConfigureSerilog method to use WebApplicationBuilder for improved integration with ASP.NET Core

--- Cv.Guard.Api/Extensions/ConfigureHostBuilderExtensions.cs
+++ Cv.Guard.Api/Extensions/ConfigureHostBuilderExtensions.cs
@@ -19,7 +19,27 @@ namespace Cv.Guard.Api.Extensions
 		/// This method sets up Serilog to log to the console with a minimum level of Debug and to a PostgreSQL database
 		/// with a minimum level of Error. The PostgreSQL table will be created automatically if it does not exist.
 		/// </remarks>
-		public static ConfigureHostBuilder ConfigureSerilog(this ConfigureHostBuilder host, string connection)
+		using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.MSSqlServer;
+using System.Data;
+
+public static class ConfigureHostBuilderExtensions
+	{
+
+		/// <summary>
+		/// Configures Serilog for the specified <see cref="ConfigureHostBuilder"/> instance.
+		/// </summary>
+		/// <param name="host">The <see cref="ConfigureHostBuilder"/> instance to configure.</param>
+		/// <param name="connection">The connection string for the PostgreSQL database.</param>
+		/// <returns>The configured <see cref="ConfigureHostBuilder"/> instance.</returns>
+		/// <remarks>
+		/// This method sets up Serilog to log to the console with a minimum level of Debug and to a PostgreSQL database
+		/// with a minimum level of Error. The PostgreSQL table will be created automatically if it does not exist.
+		/// </remarks>
+		public static WebApplicationBuilder ConfigureSerilog(this WebApplicationBuilder builder, string connection)
 		{
 			var columnOptions = new ColumnOptions
 			{
@@ -29,28 +49,30 @@ namespace Cv.Guard.Api.Extensions
 				MessageTemplate = { ColumnName = "MessageTemplate", DataType = SqlDbType.NVarChar, DataLength = -1 },
 				Message = { ColumnName = "Message", DataType = SqlDbType.NVarChar, DataLength = -1 }
 			};
+
 			var sinkOptions = new MSSqlServerSinkOptions
 			{
-				AutoCreateSqlTable = true, 
+				AutoCreateSqlTable = true,
 				TableName = "Logs",
 				SchemaName = "audit",
 			};
-			Serilog.Debugging.SelfLog.Enable(Console.Error);
-		
-		   host.UseSerilog((context, services, loggerConfiguration) =>
-			   loggerConfiguration
-				   .Enrich.FromLogContext()
-				   .Enrich.WithProperty("MachineId", Environment.MachineName)
-				   .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Debug)
-				   .WriteTo.MSSqlServer(
-					   connectionString: connection,
-					   sinkOptions: sinkOptions,
-					   columnOptions: columnOptions,
-					   restrictedToMinimumLevel: LogEventLevel.Error
-				   )
-		   )
-			return host;
-		}
 
+			Serilog.Debugging.SelfLog.Enable(Console.Error);
+
+			builder.Host.UseSerilog((context, services, loggerConfiguration) =>
+				loggerConfiguration
+					.Enrich.FromLogContext()
+					.Enrich.WithProperty("MachineId", Environment.MachineName)
+					.WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Debug)
+					.WriteTo.MSSqlServer(
+						connectionString: connection,
+						sinkOptions: sinkOptions,
+						columnOptions: columnOptions,
+						restrictedToMinimumLevel: LogEventLevel.Error
+					)
+			);
+
+			return builder;
+		}
 	}
 }
