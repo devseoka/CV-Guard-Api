0296e162107c46728e92d627e1dbb27df9334097
Moshele Seoka <seokamk@yahoo.com>
2025-02-08 13:12:59 +0200

    Fix: Resolve Schema Creation And

--- Cv.Guard.Api/Extensions/ServiceProviderExtensions.cs
+++ Cv.Guard.Api/Extensions/ServiceProviderExtensions.cs
@@ -9,7 +9,8 @@ namespace Cv.Guard.Api.Extensions
             
             using var scope = services.CreateScope();
             var context = scope.ServiceProvider.GetRequiredService<CvgaContext>();
-			context.Database.ExecuteSqlRaw("CREATE SCHEMA IF NOT EXISTS audit");
+			context.Database.ExecuteSqlRaw(@"IF NOT EXISTS (SELECT 1 FROM sys.schemas WHERE name = 'audit')
+			BEGIN EXEC('CREATE SCHEMA audit'); END");
 			context.Database.Migrate();
             return services;
         }
