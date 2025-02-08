1cb1fa6a5183447baeaff23bf61c2694c94b5981
Moshele Seoka <seokamk@yahoo.com>
2025-02-08 19:48:45 +0200

    Feat: Added New Health Checks For Database

--- Cv.Guard.Api/Program.cs
+++ Cv.Guard.Api/Program.cs
@@ -1,9 +1,12 @@
+using Azure.Storage.Blobs;
 using Cv.Guard.Api.Extensions;
 using Cv.Guard.Api.Helpers.Middleware;
 using FluentValidation;
+using HealthChecks.UI.Client;
 using IpStack.Extensions;
 using IpStack.Models;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Serilog;
 using System.Reflection;
 using System.Text;
@@ -35,10 +38,22 @@ try
 
 	builder.Services.ConfigureSwagger();
 	builder.ConfigureSerilog(connection);
+	builder.Services.AddHealthChecks()
+	.AddSqlServer(
+		connectionString: builder.Configuration.GetConnectionString("DefaultConnection"),
+		name: "SQL Server",
+		failureStatus: HealthStatus.Unhealthy,
+		tags: ["database", "sql"])
+	.AddAzureBlobStorage(
+		name: "Azure Blob Storage",
+		failureStatus: HealthStatus.Unhealthy,
+		tags: ["storage", "azure"],
+		clientFactory: serviceProvider => serviceProvider.GetRequiredService<BlobServiceClient>());
 
 	var app = builder.Build();
-	
-	app.UseForwardedHeaders(new () {
+
+	app.UseForwardedHeaders(new()
+	{
 		ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
 	});
 	app.UseSerilogRequestLogging();
@@ -57,6 +72,10 @@ try
 	app.Services.EnsureDatabaseCreated();
 
 	app.MapControllers();
+
+	app.MapHealthChecks("/api/health", new(){
+		ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+	});
 
 	app.Run();
 
